9e501773a56d0f5df978d6a60f72b7ae82ec297c
井上謙太郎 <yasusi.1530@gmail.com>
2024-01-10 05:02:03 +0900

    js-test.html テスト

--- js/js-test.js
+++ js/js-test.js
@@ -1,17 +1,17 @@
-// import loadModule from './loadModule.js';
-const loadModule = async (filepath)=>{
-    return await import(filepath).then(module=>{
-        for( const [key, val] of Object.entries(module) ){
-            if( window[key]==null ){
-                window[key]=val;
-                console.log('Set Global Property '+key);
-            }
-            else{
-                console.error('! Global.'+key+' already defined');
-            }
-        }
-    });
-}
+import loadModule from './loadModule.js';
+// const loadModule = async (filepath)=>{
+//     return await import(filepath).then(module=>{
+//         for( const [key, val] of Object.entries(module) ){
+//             if( window[key]==null ){
+//                 window[key]=val;
+//                 console.log('Set Global Property '+key);
+//             }
+//             else{
+//                 console.error('! Global.'+key+' already defined');
+//             }
+//         }
+//     });
+// }
 
 import aceUtil from './aceUtil.js';
 
@@ -33,7 +33,7 @@ const modules = [
 
 window.addEventListener('DOMContentLoaded', ()=>{
     console.log('===== js/js-test.js DOMCContentLoaded START =====');
-    console.log('bbb');
+    console.log('ccc');
     aceUtil.set('editor', 'ace/mode/javascript', 'ace/theme/monokai');
     aceUtil.setValue('output.log("Hello World!")')
 

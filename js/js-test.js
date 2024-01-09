bd25dfa2b4e9b0d80d79dd9e0ff3f82b54e4e1ee
井上謙太郎 <yasusi.1530@gmail.com>
2024-01-10 04:48:18 +0900

    js-test.html テスト

--- js/js-test.js
+++ js/js-test.js
@@ -1,46 +1,46 @@
 // import loadModule from './loadModule.js';
-// const loadModule = async (filepath)=>{
-//     return await import(filepath).then(module=>{
-//         for( const [key, val] of Object.entries(module) ){
-//             if( window[key]==null ){
-//                 window[key]=val;
-//                 console.log('Set Global Property '+key);
-//             }
-//             else{
-//                 console.error('! Global.'+key+' already defined');
-//             }
-//         }
-//     });
-// }
+const loadModule = async (filepath)=>{
+    return await import(filepath).then(module=>{
+        for( const [key, val] of Object.entries(module) ){
+            if( window[key]==null ){
+                window[key]=val;
+                console.log('Set Global Property '+key);
+            }
+            else{
+                console.error('! Global.'+key+' already defined');
+            }
+        }
+    });
+}
 
 import aceUtil from './aceUtil.js';
 
-const output={
-    log: (...args)=>{
-        const elem=document.getElementById('result');
-        const div=document.createElement('div');
-        const text=args.reverse().reduce((a, sum)=> sum+' '+a, '');
+// const output={
+//     log: (...args)=>{
+//         const elem=document.getElementById('result');
+//         const div=document.createElement('div');
+//         const text=args.reverse().reduce((a, sum)=> sum+' '+a, '');
 
-        div.innerHTML=text;
-        elem.appendChild(div);
-    }
-};
+//         div.innerHTML=text;
+//         elem.appendChild(div);
+//     }
+// };
 
-window['output']=output;
-// const modules = [
-//     loadModule('./output.js')
-// ];
+// window['output']=output;
+const modules = [
+    loadModule('./output.js')
+];
 
 window.addEventListener('DOMContentLoaded', ()=>{
     console.log('===== js/js-test.js DOMCContentLoaded START =====');
     aceUtil.set('editor', 'ace/mode/javascript', 'ace/theme/monokai');
     aceUtil.setValue('output.log("Hello World!")')
 
-    run();
+//    run();
     // Promise.all(modules).then(()=>{
     // 	run();
     // });
-//    setTimeout(()=> run(), 1000);
+    setTimeout(()=> run(), 1000);
     
     console.log('===== js/js-test.js DOMCContentLoaded FINISH =====');
 });

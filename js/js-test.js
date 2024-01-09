569f59440fadc5aba40594d0fd3d8d5a67ef5ebc
井上謙太郎 <yasusi.1530@gmail.com>
2024-01-09 14:50:05 +0900

    js-test.html メイン部分実装

--- js/js-test.js
+++ js/js-test.js
@@ -1,11 +1,19 @@
+import loadModule from './loadModule.js';
 import aceUtil from './aceUtil.js';
+
+const modules = [
+    loadModule('./output.js')
+];
 
 window.addEventListener('DOMContentLoaded', ()=>{
     console.log('===== js/js-test.js DOMCContentLoaded START =====');
     aceUtil.set('editor', 'ace/mode/javascript', 'ace/theme/monokai');
-    aceUtil.setValue('console.log("Hello World!")')
+    aceUtil.setValue('output.log("Hello World!")')
 
-    run();
+    Promise.all(modules).then(()=>{
+	run();
+    });
+//    setTimeout(()=> run(), 1000);
     
     function run(){
         const f=Function(aceUtil.getValue());

e014bfbec7cae9dd9758beee53bcf49824414046
井上謙太郎 <yasusi.1530@gmail.com>
2024-01-09 16:10:38 +0900

    js-test.html テスト

--- js/js-test.js
+++ js/js-test.js
@@ -1,20 +1,32 @@
 // import loadModule from './loadModule.js';
-const loadModule = async (filepath)=>{
-    return await import(filepath).then(module=>{
-        for( const [key, val] of Object.entries(module) ){
-            if( window[key]==null ){
-                window[key]=val;
-                console.log('Set Global Property '+key);
-            }
-            else{
-                console.error('! Global.'+key+' already defined');
-            }
-        }
-    });
-}
+// const loadModule = async (filepath)=>{
+//     return await import(filepath).then(module=>{
+//         for( const [key, val] of Object.entries(module) ){
+//             if( window[key]==null ){
+//                 window[key]=val;
+//                 console.log('Set Global Property '+key);
+//             }
+//             else{
+//                 console.error('! Global.'+key+' already defined');
+//             }
+//         }
+//     });
+// }
 
 import aceUtil from './aceUtil.js';
 
+const output={
+    log: (...args)=>{
+        const elem=document.getElementById('result');
+        const div=document.createElement('div');
+        const text=args.reverse().reduce((a, sum)=> sum+' '+a, '');
+
+        div.innerHTML=text;
+        elem.appendChild(div);
+    }
+};
+
+window['output']=output;
 // const modules = [
 //     loadModule('./output.js')
 // ];
@@ -22,7 +34,7 @@ import aceUtil from './aceUtil.js';
 window.addEventListener('DOMContentLoaded', ()=>{
     console.log('===== js/js-test.js DOMCContentLoaded START =====');
     aceUtil.set('editor', 'ace/mode/javascript', 'ace/theme/monokai');
-    aceUtil.setValue('result.log("Hello World!")')
+    aceUtil.setValue('output.log("Hello World!")')
 
     run();
     // Promise.all(modules).then(()=>{

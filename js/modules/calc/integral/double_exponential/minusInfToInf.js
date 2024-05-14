259f82d1cc8b1c25be3af124b86e9c3516eb73d4
井上謙太郎 <yasusi.1530@gmail.com>
2024-05-15 08:06:47 +0900

    js/test/orthogonal/laguerre.js 完成

--- js/modules/calc/integral/double_exponential/minusInfToInf.js
+++ js/modules/calc/integral/double_exponential/minusInfToInf.js
@@ -17,19 +17,11 @@ const TEST_VALUE=1.0e8;
 const TEST_THRE=1.0e-8;
 
 export default (f, thre=1.0e-8)=>{
-//    const convFunc=t=> f(sinh(mul(0.5*Math.PI, sinh(t))));
-//    const weight=t=>mul(0.5*Math.PI, cosh(t), cosh(mul(0.5*Math.PI, sinh(t))));
     if( f(TEST_VALUE) || f(minus(TEST_VALUE)) ) throw new Error('!!! integral.minusInfToInf Function f(x) must not diverge !!!');
     
     const integradFunc=t=> mul(f(sinh(mul(0.5*Math.PI, sinh(t)))), mul(0.5*Math.PI, cosh(t), cosh(mul(0.5*Math.PI, sinh(t)))));
     const innerIntegral=x=> trapezoid(integradFunc, minus(x), x);
     
-    console.log(f(TEST_VALUE));
-    console.log(f(minus(TEST_VALUE)));
-
-    console.log(integradFunc(TEST_VALUE));
-    console.log(integradFunc(minus(TEST_VALUE)));
-
     let max_range=TEST_VALUE;
     while( Number.isNaN(integradFunc(max_range)) || Number.isNaN(integradFunc(minus(max_range))) ) max_range*=0.5;
     
@@ -46,35 +38,18 @@ export default (f, thre=1.0e-8)=>{
     console.log(range);
 
     let val1=innerIntegral(range), val2=innerIntegral(range+step);
-    while( abs(val1-val2)>thre ){
+    while( true ){
 	range+=step;
 	val1=val2;
 	val2=innerIntegral(range+step);
 	console.log(range, val1, val2, abs(val1-val2), thre);
+	if( abs(val1)>1 && abs((val1-val2)/val1)<thre ) break;
+	if( abs(val1-val2)<thre ) break;
+	
 	if( range>max_range ) throw new Error('!!! integral.minusInfToInf not converged !!!');
     }
 
     return val2;
-
-
-
-
-    
-    throw new Error('temp');
-    
-    
-    let x_max=6.75;
-    while( Number.isNaN(convFunc(x_max)) || Number.isNaN(convFunc(minus(x_max))) ) x_max=sub(x_max, 1);
-    let x=x_max;
-    while( abs(convFunc(x))<thre && abs(convFunc(minus(x)))<thre ) x=sub(x, 0.01);
-
-    const h=div(sub(x_max, x), 10);
-    let val=trapezoid(t=> mul(convFunc(t), weight(t)), minus(x), x);
-    let next=mul(h, add(mul(convFunc(x), weight(x)), mul(convFunc(minus(x)), weight(minus(x)))));
-    while( abs(next)> thre ){
-	val=add(val, next);
-	x=add(x, h);
-	next=mul(h, add(mul(convFunc(x), weight(x)), mul(convFunc(minus(x)), weight(minus(x)))));
-    }
-    return val;
 }
+
+
--- js/modules/calc/specialFunc/laguerre.js
+++ js/modules/calc/specialFunc/laguerre.js
@@ -2,6 +2,7 @@ import add from '../add.js'
 import sub from '../sub.js'
 import mul from '../mul.js'
 import div from '../div.js'
+import minus from '../minus.js'
 
 export default (n, x)=>{
     if( n===0 ) return 1;
@@ -12,7 +13,8 @@ export default (n, x)=>{
         for( let i=2; i<n; i++ ){
             L_n0=L_n1;
             L_n1=L_n2;
-	    L_n2=div(sub(mul(sub(2*i+1, x), L_n1), mul(i, L_n0)), i+1);
+	    L_n2=div(sub(mul(add(2*i, 1, minus(x)),L_n1), mul(i, L_n0)), i+1);
+//	    L_n2=sub(mul(add(2*i, 1, minus(x)),L_n1), mul(i, L_n0));
         }
         return L_n2;
     }    

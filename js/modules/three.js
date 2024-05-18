f80a4aba9548a332b1f8230a07335602fc66f9de
井上謙太郎 <yasusi.1530@gmail.com>
2024-05-18 21:05:22 +0900

    js/modules/three.js importエラー修正

--- js/modules/three.js
+++ js/modules/three.js
@@ -1,4 +1,4 @@
-import * as THREE from '../node_modules/three/build/three.module.js'
+import * as THREE from '../modules/three/three.module.js'
 import { OrbitControls } from './three/OrbitControls.js'
 
 export {

8bb233b9cf338781b2fcbc2774890dbebaa03f44
井上謙太郎 <yasusi.1530@gmail.com>
2024-06-14 16:02:43 +0900

    js/module/calc/specialFunc/asociatedLegendre.js 実装 いくつかバグあり

--- js/modules/calc/specialFunc/asociatedLegendre.js
+++ js/modules/calc/specialFunc/asociatedLegendre.js
@@ -2,34 +2,45 @@ import add from '../add.js'
 import sub from '../sub.js'
 import mul from '../mul.js'
 import div from '../div.js'
+
+import abs from '../abs.js'
 import pow from '../pow.js'
 
-export default (l, m, x)=>{
-    if( m<0 ) return innerCalc(-m, l, x);
-    else return innerCalc(m, l, x);
+export default (m, l, x)=>{
+    if( !Number.isInteger(l) || !Number.isInteger(m) ) throw new Error('!!! asociatedLegendre(m, l, x)   l, m must be Interger !!!');
+    if( l<0 ) throw new Error('!!! asociatedLendre is not support l<0 !!!');
+
+    const m2=abs(m);
+    if( m2>l ) return 0;
     
-}
-function innerCalc(m, l, x){
-    if( l<m ) return 0;
-    else if( m===l ){
-	let a=1;
-	for( let i=1; i<=2*m-1; i+=2 ) a*=i;
-	return mul(a, pow(sub(1, mul(x,x)), 0.5*m))
+    if( m2===l===0 ) return 1;
+    
+    let a=1;
+    for( let i=1; i<=2*m-1; i+=2 ) a*=i;
+
+    let P_l2=0;
+    let P_l1=mul(a, pow(sub(1, mul(x, x)), 0.5*m2));
+    if( m2===l ) return mul(parityFactor(l, m), P_l1);
+    let P_l0=mul(2*m2+1, x, P_l1);
+        
+    for( let i=m2+1; i<l; i++ ){
+	P_l2=P_l1;
+	P_l1=P_l0;
+
+//	console.log('i=', i, i-m2, 2*i-1, P_l1, i-1+m2, P_l2);
+	P_l0=div(sub( mul(2*i+1, x, P_l1), mul(i+m2, P_l2)), i-m2+1);
     }
-    else{
-	let a=1;
-	for( let i=1; i<=2*m-1; i+=2 ) a*=i;
-	let P_n0=0, P_n1=mul(a, pow(sub(1, mul(x, x)), 0.5*m));
-	let P_n2=mul(2*m+1, x, P_n1);
-//	console.log(P_n1, P_n2);
-	
-	for( let i=m+1; i<l; i++ ){
-            P_n0=P_n1;
-            P_n1=P_n2;
-//	    console.log('aaa', i);
-//	    P_n2=((2*i+3)*x*P_n1-(l+m+1)*P_n0)/(i+2-m);
-	    P_n2=div(sub(mul(2*i+1, x, P_n1), mul(i+m, P_n0)), i+1-m);
-	}
-	return P_n2;
-    }
+    return mul(parityFactor(l, m), P_l0);
 }
+
+function parityFactor(l, m){
+    if( m>=0 ) return 1;
+
+    let deno=1;
+    for( let i=l+m+1; i<=l-m; i++ ) deno*=i;
+
+//    console.log(l, m, deno);
+    if( m%2===0 ) return 1/deno;
+    else return -1/deno;
+}
+    
--- js/test/specialFunc/orthogonal/legendre.js
+++ js/test/specialFunc/orthogonal/legendre.js
@@ -6,7 +6,7 @@ for( let i=0; i<N; i++ ){
 	const val=integral.simpson((x)=> specialFunc.legendre(i, x)*specialFunc.legendre(j, x) , -1, 1);
 	const true_val=i===j ? 2/(2*i+1) : 0;
 	
-	if( abs(val-true_val)<THRESHOLD ){
+	if( abs(val-true_val)>THRESHOLD ){
 	    output.math(`\\int^1_{-1} P_{${i}}(x) P_{${j}}(x) dx =`, val, '\\hspace{5mm} true =', true_val);  
 	}
     }
